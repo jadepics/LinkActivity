d779bdc1357aa85d50d22e31572fd4d62407251b
Alessandro <alessandrolori179@gmail.com>
2022-12-15 09:39:35 +0100

    Commit

--- Codice/Link-Activity/src/main/java/linkactivity/linkactivity/RegistrationGraphicController.java
+++ Codice/Link-Activity/src/main/java/linkactivity/linkactivity/RegistrationGraphicController.java
@@ -6,15 +6,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class RegistrationGraphicController {
 
     @FXML
     private Button backButton1;
+
+    @FXML
+    private TextField regEmailUser;
 
     @FXML
     private Button loginButton2;
@@ -68,6 +73,36 @@ public class RegistrationGraphicController {
     void signUpGoogle() /*throws IOException*/{
         //todo holy shit this shit has to be done SIGNUPGOOGLE
         System.out.println("Holy shit this shit has to be done SIGNUPGOOGLE");
+    }
+
+    @FXML
+    void regEmailUser() {
+        String str;
+        int a=0,b=0;
+        String[] pars = new String[10];
+        str= regEmailUser.getText();
+        pars[1]=str;
+
+        String[] result = str.split("\\a");
+        for (int i = 0;i < result[0].length(); i++){
+            pars[0]= String.valueOf(result[0].charAt(i)); //parsa la parola in lettere per controllare se c'è la @
+            if(pars[0].equals("@")){
+                a=1;
+            }
+            if(a==1){
+                if(pars[0].equals(".")){
+                    b=1;
+                }
+            }
+            //System.out.println(pars[0]);
+        }
+        if(a==0 || b==0){
+            System.out.println("Direi quello che direbbe Germano Mosconi");
+        } else {
+            System.out.println("ye");
+        }
+        //System.out.println(c[1]);
+        //System.out.println(str);
     }
 
 }
