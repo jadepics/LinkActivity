eae7717d0f072f161f1363125d0a82d19e3b4907
jadepics <116430207+jadepics@users.noreply.github.com>
2023-02-07 15:50:03 +0100

    Commit

---
+++ Codice/Link-Activity/src/main/java/linkactivity/linkactivity/EventBean.java
@@ -0,0 +1,75 @@
+package linkactivity.linkactivity;
+
+import java.util.Date;
+
+public class EventBean {
+    String eventName;
+    Date dataEvento;
+    Date expirationDate;
+    String Description;
+    int partecipantNumber;
+    String nomeAzienda;
+    String tag;
+
+
+    public EventBean(){}    //costruttore vuoto
+
+    public EventBean(String eventName,Date dataEvento,Date expirationDate,String Description,int partecipantNumber,String nomeAzienda, String tag ){}
+
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getDataEvento() {
+        return dataEvento;
+    }
+
+    public void setDataEvento(Date dataEvento) {
+        this.dataEvento = dataEvento;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public int getPartecipantNumber() {
+        return partecipantNumber;
+    }
+
+    public void setPartecipantNumber(int partecipantNumber) {
+        this.partecipantNumber = partecipantNumber;
+    }
+
+    public String getNomeAzienda() {
+        return nomeAzienda;
+    }
+
+    public void setNomeAzienda(String nomeAzienda) {
+        this.nomeAzienda = nomeAzienda;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
