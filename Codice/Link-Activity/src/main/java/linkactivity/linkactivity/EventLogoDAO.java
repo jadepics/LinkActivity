f1c7f545c19054c561639161726d6b14dbbae49c
Alessandro <alessandrolori179@gmail.com>
2023-06-05 16:52:14 +0200

    Commit

--- Codice/Link-Activity/src/main/java/linkactivity/linkactivity/EventLogoDAO.java
+++ Codice/Link-Activity/src/main/java/linkactivity/linkactivity/EventLogoDAO.java
@@ -8,40 +8,36 @@ public class EventLogoDAO {
         String nomeaz= eventModel.getEventModelNomeAzienda();
         File file= new File("src/main/CompanyLogo-Filesystem.txt");
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
 
-        String line;
-        boolean foundCompany = false;
-        String companyString = "";
-        StringBuilder sb = new StringBuilder();
+            String line;
+            boolean foundCompany = false;
+            String companyString = "";
+            StringBuilder sb = new StringBuilder();
 
-        // Lettura del file riga per riga
-        while ((line = reader.readLine()) != null) {
-            // Se la riga contiene "IBM", copia la stringa successiva
-            if (!foundCompany && line.contains(nomeaz)) {
-                companyString = line.substring(line.indexOf(nomeaz) + nomeaz.length()+1).trim();
-                foundCompany = true;
-            } else if (foundCompany) {
-                // Scandisce la stringa per cercare il carattere "\" e aggiunge un secondo "\"
-
-                for (char h : companyString.toCharArray()) {
-                    sb.append(h);
-                    if (h == '\\') {
-                        sb.append('\\');
+            while ((line = reader.readLine()) != null) {
+                if (!foundCompany && line.contains(nomeaz)) {
+                    companyString = line.substring(line.indexOf(nomeaz) + nomeaz.length() + 1).trim();
+                    foundCompany = true;
+                } else if (foundCompany) {
+                    for (char h : companyString.toCharArray()) {
+                        sb.append(h);
+                        if (h == '\\') {
+                            sb.append('\\');
+                        }
                     }
+                    break;
                 }
-                break;
             }
+            eventModel = new EventModel(String.valueOf(sb)) {
+            };
+            return eventModel;
         }
-        reader.close();
-
-        eventModel= new EventModel(String.valueOf(sb)){};
-        return eventModel;
     }
 
     public void insertLogo(String imagePath, CompanyBean companyName) {
         String content= companyName.getNomeAzienda() +" " + imagePath;
-        try (FileWriter fileWriter = new FileWriter("src/main/CompanyLogo-Filesystem.txt", true)) {
+        try (FileWriter fileWriter = new FileWriter("src/main/CompanyLogo-Filesystem.txt ", true)) {
             fileWriter.write(content);
             fileWriter.write(System.lineSeparator());
         } catch (IOException e) {
